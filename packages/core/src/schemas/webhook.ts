90236fe3a88828997499d72cf2ed5a837227b17b
Ambuj Kumar <ambuj@ghewar.local>
2025-07-06 03:38:47 +0530

    feat(core): improve type safety and usage with zod schemas
    - also avoid using Promise.all which does not handle errors correctly

---
+++ packages/core/src/schemas/webhook.ts
@@ -0,0 +1,392 @@
+import { z } from "zod";
+
+export const PaymentSchema = z.object({
+  payload_type: z.literal("Payment"),
+  billing: z.object({
+    city: z.string().nullable(),
+    country: z.string().nullable(),
+    state: z.string().nullable(),
+    street: z.string().nullable(),
+    zipcode: z.string().nullable(),
+  }),
+  brand_id: z.string(),
+  business_id: z.string(),
+  card_issuing_country: z.string().nullable(),
+  card_last_four: z.string().nullable(),
+  card_network: z.string().nullable(),
+  card_type: z.string().nullable(),
+  created_at: z.string().transform((d) => new Date(d)),
+  currency: z.string(),
+  customer: z.object({
+    customer_id: z.string(),
+    email: z.string(),
+    name: z.string().nullable(),
+  }),
+  digital_products_delivered: z.boolean(),
+  discount_id: z.string().nullable(),
+  disputes: z
+    .array(
+      z.object({
+        amount: z.string(),
+        business_id: z.string(),
+        created_at: z.string().transform((d) => new Date(d)),
+        currency: z.string(),
+        dispute_id: z.string(),
+        dispute_stage: z.enum([
+          "pre_dispute",
+          "dispute_opened",
+          "dispute_won",
+          "dispute_lost",
+        ]),
+        dispute_status: z.enum([
+          "dispute_opened",
+          "dispute_won",
+          "dispute_lost",
+          "dispute_accepted",
+          "dispute_cancelled",
+          "dispute_challenged",
+        ]),
+        payment_id: z.string(),
+        remarks: z.string().nullable(),
+      }),
+    )
+    .nullable(),
+  error_code: z.string().nullable(),
+  error_message: z.string().nullable(),
+  metadata: z.record(z.any()).nullable(),
+  payment_id: z.string(),
+  payment_link: z.string().nullable(),
+  payment_method: z.string().nullable(),
+  payment_method_type: z.string().nullable(),
+  product_cart: z
+    .array(
+      z.object({
+        product_id: z.string(),
+        quantity: z.number(),
+      }),
+    )
+    .nullable(),
+  refunds: z
+    .array(
+      z.object({
+        amount: z.number(),
+        business_id: z.string(),
+        created_at: z.string().transform((d) => new Date(d)),
+        currency: z.string(),
+        is_partial: z.boolean(),
+        payment_id: z.string(),
+        reason: z.string().nullable(),
+        refund_id: z.string(),
+        status: z.enum(["succeeded", "failed", "pending"]),
+      }),
+    )
+    .nullable(),
+  settlement_amount: z.number(),
+  settlement_currency: z.string(),
+  settlement_tax: z.number().nullable(),
+  status: z.enum(["succeeded", "failed", "pending", "processing", "cancelled"]),
+  subscription_id: z.string().nullable(),
+  tax: z.number().nullable(),
+  total_amount: z.number(),
+  updated_at: z
+    .string()
+    .transform((d) => new Date(d))
+    .nullable(),
+});
+
+export const SubscriptionSchema = z.object({
+  payload_type: z.literal("Subscription"),
+  addons: z
+    .array(
+      z.object({
+        addon_id: z.string(),
+        quantity: z.number(),
+      }),
+    )
+    .nullable(),
+  billing: z.object({
+    city: z.string().nullable(),
+    country: z.string().nullable(),
+    state: z.string().nullable(),
+    street: z.string().nullable(),
+    zipcode: z.string().nullable(),
+  }),
+  cancel_at_next_billing_date: z.boolean(),
+  cancelled_at: z
+    .string()
+    .transform((d) => new Date(d))
+    .nullable(),
+  created_at: z.string().transform((d) => new Date(d)),
+  currency: z.string(),
+  customer: z.object({
+    customer_id: z.string(),
+    email: z.string(),
+    name: z.string().nullable(),
+  }),
+  discount_id: z.string().nullable(),
+  metadata: z.record(z.any()).nullable(),
+  next_billing_date: z
+    .string()
+    .transform((d) => new Date(d))
+    .nullable(),
+  on_demand: z.boolean(),
+  payment_frequency_count: z.number(),
+  payment_frequency_interval: z.enum(["Day", "Week", "Month", "Year"]),
+  previous_billing_date: z
+    .string()
+    .transform((d) => new Date(d))
+    .nullable(),
+  product_id: z.string(),
+  quantity: z.number(),
+  recurring_pre_tax_amount: z.number(),
+  status: z.enum([
+    "pending",
+    "active",
+    "on_hold",
+    "paused",
+    "cancelled",
+    "expired",
+    "failed",
+  ]),
+  subscription_id: z.string(),
+  subscription_period_count: z.number(),
+  subscription_period_interval: z.enum(["Day", "Week", "Month", "Year"]),
+  tax_inclusive: z.boolean(),
+  trial_period_days: z.number(),
+});
+
+export const RefundSchema = z.object({
+  payload_type: z.literal("Refund"),
+  amount: z.number(),
+  business_id: z.string(),
+  created_at: z.string().transform((d) => new Date(d)),
+  currency: z.string(),
+  is_partial: z.boolean(),
+  payment_id: z.string(),
+  reason: z.string().nullable(),
+  refund_id: z.string(),
+  status: z.enum(["succeeded", "failed", "pending"]),
+});
+
+export const DisputeSchema = z.object({
+  payload_type: z.literal("Dispute"),
+  amount: z.string(),
+  business_id: z.string(),
+  created_at: z.string().transform((d) => new Date(d)),
+  currency: z.string(),
+  dispute_id: z.string(),
+  dispute_stage: z.enum([
+    "pre_dispute",
+    "dispute_opened",
+    "dispute_won",
+    "dispute_lost",
+  ]),
+  dispute_status: z.enum([
+    "dispute_opened",
+    "dispute_won",
+    "dispute_lost",
+    "dispute_accepted",
+    "dispute_cancelled",
+    "dispute_challenged",
+  ]),
+  payment_id: z.string(),
+  remarks: z.string().nullable(),
+});
+
+export const LicenseKeySchema = z.object({
+  payload_type: z.literal("LicenseKey"),
+  activations_limit: z.number(),
+  business_id: z.string(),
+  created_at: z.string().transform((d) => new Date(d)),
+  customer_id: z.string(),
+  expires_at: z
+    .string()
+    .transform((d) => new Date(d))
+    .nullable(),
+  id: z.string(),
+  instances_count: z.number(),
+  key: z.string(),
+  payment_id: z.string(),
+  product_id: z.string(),
+  status: z.enum(["active", "inactive", "expired"]),
+  subscription_id: z.string().nullable(),
+});
+
+export const PaymentSucceededPayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("payment.succeeded"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: PaymentSchema,
+});
+
+export const PaymentFailedPayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("payment.failed"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: PaymentSchema,
+});
+
+export const PaymentProcessingPayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("payment.processing"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: PaymentSchema,
+});
+
+export const PaymentCancelledPayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("payment.cancelled"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: PaymentSchema,
+});
+
+export const RefundSucceededPayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("refund.succeeded"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: RefundSchema,
+});
+
+export const RefundFailedPayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("refund.failed"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: RefundSchema,
+});
+
+export const DisputeOpenedPayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("dispute.opened"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: DisputeSchema,
+});
+
+export const DisputeExpiredPayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("dispute.expired"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: DisputeSchema,
+});
+
+export const DisputeAcceptedPayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("dispute.accepted"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: DisputeSchema,
+});
+
+export const DisputeCancelledPayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("dispute.cancelled"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: DisputeSchema,
+});
+
+export const DisputeChallengedPayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("dispute.challenged"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: DisputeSchema,
+});
+
+export const DisputeWonPayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("dispute.won"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: DisputeSchema,
+});
+
+export const DisputeLostPayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("dispute.lost"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: DisputeSchema,
+});
+
+export const SubscriptionActivePayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("subscription.active"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: SubscriptionSchema,
+});
+
+export const SubscriptionOnHoldPayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("subscription.on_hold"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: SubscriptionSchema,
+});
+
+export const SubscriptionRenewedPayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("subscription.renewed"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: SubscriptionSchema,
+});
+
+export const SubscriptionPausedPayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("subscription.paused"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: SubscriptionSchema,
+});
+
+export const SubscriptionPlanChangedPayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("subscription.plan_changed"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: SubscriptionSchema,
+});
+
+export const SubscriptionCancelledPayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("subscription.cancelled"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: SubscriptionSchema,
+});
+
+export const SubscriptionFailedPayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("subscription.failed"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: SubscriptionSchema,
+});
+
+export const SubscriptionExpiredPayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("subscription.expired"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: SubscriptionSchema,
+});
+
+export const LicenseKeyCreatedPayloadSchema = z.object({
+  business_id: z.string(),
+  type: z.literal("license_key.created"),
+  timestamp: z.string().transform((d) => new Date(d)),
+  data: LicenseKeySchema,
+});
+
+export const WebhookPayloadSchema = z.discriminatedUnion("type", [
+  PaymentSucceededPayloadSchema,
+  PaymentFailedPayloadSchema,
+  PaymentProcessingPayloadSchema,
+  PaymentCancelledPayloadSchema,
+  RefundSucceededPayloadSchema,
+  RefundFailedPayloadSchema,
+  DisputeOpenedPayloadSchema,
+  DisputeExpiredPayloadSchema,
+  DisputeAcceptedPayloadSchema,
+  DisputeCancelledPayloadSchema,
+  DisputeChallengedPayloadSchema,
+  DisputeWonPayloadSchema,
+  DisputeLostPayloadSchema,
+  SubscriptionActivePayloadSchema,
+  SubscriptionOnHoldPayloadSchema,
+  SubscriptionRenewedPayloadSchema,
+  SubscriptionPausedPayloadSchema,
+  SubscriptionPlanChangedPayloadSchema,
+  SubscriptionCancelledPayloadSchema,
+  SubscriptionFailedPayloadSchema,
+  SubscriptionExpiredPayloadSchema,
+  LicenseKeyCreatedPayloadSchema,
+]);
---
+++ packages/core/src/webhook/index.ts
@@ -0,0 +1,111 @@
+import { WebhookEventHandlers, WebhookPayload, Resolve } from "../types";
+
+export type WebhookHandlerConfig = Resolve<
+  {
+    webhookKey: string;
+    onPayload?: (payload: WebhookPayload) => Promise<void>;
+  } & WebhookEventHandlers
+>;
+
+export const handleWebhookPayload = async (
+  payload: WebhookPayload,
+  config: WebhookHandlerConfig,
+) => {
+  if (config.onPayload) {
+    await config.onPayload(payload);
+  }
+
+  if (payload.type === "payment.succeeded" && config.onPaymentSucceeded) {
+    await config.onPaymentSucceeded(payload);
+  }
+
+  if (payload.type === "payment.failed" && config.onPaymentFailed) {
+    await config.onPaymentFailed(payload);
+  }
+
+  if (payload.type === "payment.processing" && config.onPaymentProcessing) {
+    await config.onPaymentProcessing(payload);
+  }
+
+  if (payload.type === "payment.cancelled" && config.onPaymentCancelled) {
+    await config.onPaymentCancelled(payload);
+  }
+
+  if (payload.type === "refund.succeeded" && config.onRefundSucceeded) {
+    await config.onRefundSucceeded(payload);
+  }
+
+  if (payload.type === "refund.failed" && config.onRefundFailed) {
+    await config.onRefundFailed(payload);
+  }
+
+  if (payload.type === "dispute.opened" && config.onDisputeOpened) {
+    await config.onDisputeOpened(payload);
+  }
+
+  if (payload.type === "dispute.expired" && config.onDisputeExpired) {
+    await config.onDisputeExpired(payload);
+  }
+
+  if (payload.type === "dispute.accepted" && config.onDisputeAccepted) {
+    await config.onDisputeAccepted(payload);
+  }
+
+  if (payload.type === "dispute.cancelled" && config.onDisputeCancelled) {
+    await config.onDisputeCancelled(payload);
+  }
+
+  if (payload.type === "dispute.challenged" && config.onDisputeChallenged) {
+    await config.onDisputeChallenged(payload);
+  }
+
+  if (payload.type === "dispute.won" && config.onDisputeWon) {
+    await config.onDisputeWon(payload);
+  }
+
+  if (payload.type === "dispute.lost" && config.onDisputeLost) {
+    await config.onDisputeLost(payload);
+  }
+
+  if (payload.type === "subscription.active" && config.onSubscriptionActive) {
+    await config.onSubscriptionActive(payload);
+  }
+
+  if (payload.type === "subscription.on_hold" && config.onSubscriptionOnHold) {
+    await config.onSubscriptionOnHold(payload);
+  }
+
+  if (payload.type === "subscription.renewed" && config.onSubscriptionRenewed) {
+    await config.onSubscriptionRenewed(payload);
+  }
+
+  if (payload.type === "subscription.paused" && config.onSubscriptionPaused) {
+    await config.onSubscriptionPaused(payload);
+  }
+
+  if (
+    payload.type === "subscription.plan_changed" &&
+    config.onSubscriptionPlanChanged
+  ) {
+    await config.onSubscriptionPlanChanged(payload);
+  }
+
+  if (
+    payload.type === "subscription.cancelled" &&
+    config.onSubscriptionCancelled
+  ) {
+    await config.onSubscriptionCancelled(payload);
+  }
+
+  if (payload.type === "subscription.failed" && config.onSubscriptionFailed) {
+    await config.onSubscriptionFailed(payload);
+  }
+
+  if (payload.type === "subscription.expired" && config.onSubscriptionExpired) {
+    await config.onSubscriptionExpired(payload);
+  }
+
+  if (payload.type === "license_key.created" && config.onLicenseKeyCreated) {
+    await config.onLicenseKeyCreated(payload);
+  }
+};
