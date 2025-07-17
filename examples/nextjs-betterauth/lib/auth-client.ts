d5debac25eaf9dcb736a6599c0b066898af0afce
Ambuj Kumar <ambuj@dodopayments.com>
2025-07-17 18:29:06 +0530

    test: fixes for betterauth example tests

--- examples/nextjs-betterauth/lib/auth-client.ts
+++ examples/nextjs-betterauth/lib/auth-client.ts
@@ -5,27 +5,3 @@ export const authClient = createAuthClient({
   baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
   plugins: [dodopaymentsClient()],
 });
-
-async function sample() {
-  // return a list of subscriptions purchased the signed in user
-  await authClient.customer.subscriptions.list({
-    query: {
-      limit: 10,
-      page: 1,
-      active: true, // default is false, omit this to get all subscriptions
-    },
-  });
-
-  // return a list of payments made by the signed in user
-  await authClient.customer.payments.list({
-    query: {
-      limit: 10,
-      page: 1,
-      status: "succeeded", // omit this to get all payments
-    },
-  });
-
-  // gives a link to the customer portal for the signed in user
-  // the developer has to do a redirect
-  await authClient.customer.portal();
-}
